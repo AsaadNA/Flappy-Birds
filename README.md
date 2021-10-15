f4ea91e572c0f4a47b87626f2223dbdf08b1b357
Asaad Abbasi <asaad.abbasi@gmail.com>
2021-10-16 00:55:08 +0500

    initial commit : port to js

---
+++ README.md
@@ -0,0 +1,10 @@
+# Flappy Birds
+
+This is the clone of the infamous game Flappy Birds ! 
+
+# Features
+
+- Infinite Pipe Generation
+- Collision detection
+- Score system
+
--- build/build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cls
-g++ ../src/flappy.cpp ../src/player.cpp ../src/level.cpp -o "Flappy Bird" -std=c++11 -lmingw32 -lsdl2main -lsdl2_image -lsdl2 -lsdl2_ttf -lsdl2_mixer
